5d82f9467cbf6165f85a16c1feff9c5dddfe21f1
Carter-Pro <18686883412@163.com>
2016-03-03 20:11:54 +0800

    Use vector
    Change array to vector

--- LeetCode/RemoveElement.cpp
+++ LeetCode/RemoveElement.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-class RemoveElement {
-public:
-    int removeelement(int arry[], int n, int value) {
-        int i = 0;
-        int j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (arry[i] == value)
-                continue;
 
-            arry[j] = arry[i];
-            j++;
+class RemoveElement 
+{
+public:
+    int removeelement(vector<int>& nums, int value) 
+    {
+        int i = nums.size() - 1;
+        int j = 0;
+        while (j <= i)
+        {
+            
         }
-        return j;
     }
 };
 int main(void)
 {
     RemoveElement test;
+
     int array[] = {1, 2, 2, 3, 2, 4};
+    vector<int> nums(array, array + 6);
+
     int result;
-    result = test.removeelement(array, 6, 2);
+    result = test.removeelement(nums, 2);
     for (int i = 0; i < result; ++i) {
-        cout << array[i] << endl;
+        cout << nums[i] << endl;
     }
     return 0;
 }
